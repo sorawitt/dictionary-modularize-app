7d8fec14f12d52be1641a754d197c49d4148d800
Sorawit Trutsat <sorawit.tr@gmail.com>
2022-02-10 14:22:56 +0700

    initial commit

---
+++ DictionaryApp/DictionaryApp/DictionaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryAppApp.swift
+//  DictionaryApp
+//
+//  Created by Sorawit Trutsat on 10/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
